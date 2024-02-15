230981b4373f94fd15669a7ebf941083f71061f8
Elyees Tatua <elyeestatua@gmail.com>
2024-02-15 19:41:10 +0300

    ops(Countries): Setting up the props function

--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -3,6 +3,11 @@ import { CountriesProps } from "./Home";
 import { Link } from "react-router-dom";
 import { setTitle } from "../helpers/setTitle";
 
-
+const Countries: React.FC<CountriesProps> = ({ countries, pages }) => {
+    //document title change
+    setTitle(
+      "Mela | Where in the World?",
+      "https://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/512/globe-icon.png"
+    );
 
 export default MainApp
