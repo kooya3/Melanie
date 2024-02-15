f8025b283d1d1783363c24e5f4501ee2ef03df4c
Elyees Tatua <elyeestatua@gmail.com>
2024-02-15 19:39:55 +0300

    ops(Countries): Working on the importation of required props

--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { CountriesProps } from "./Home";
 import { Link } from "react-router-dom";
+import { setTitle } from "../helpers/setTitle";
 
 const MainApp = () => {
   return (
