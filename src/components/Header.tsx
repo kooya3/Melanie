be389b8676f3db8acf2c38490cad0999bf0ae1ab
Elyees Tatua <elyeestatua@gmail.com>
2024-02-15 21:46:33 +0300

    ops(Header): Created themes for the component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,6 @@ import { sun } from "react-icons-kit/icomoon/sun";
 import { moonO } from "react-icons-kit/fa/moonO";
 import { Link } from "react-router-dom";
 
-
 const setTheme = (darkMode: DarkModeContext) => {
   const isDark = darkMode.mode.isDark;
   darkMode.dispatch(!isDark);
@@ -20,8 +19,7 @@ const Header = () => {
         "z-10 shadow-md p-8 px-[2rem] md:px-[4rem] transition-colors items-center sticky"
       }
     >
-
-<Link to={`/where-in-the-world`}>
+      <Link to={`/where-in-the-world`}>
         <h1 className="text-xl sm:text-2xl xl:text-[24px] tracking-[1px] font-extrabold w-[8rem] sm:w-auto">
           Hi Melanie! Can We play Where in the World?
         </h1>
@@ -30,4 +28,11 @@ const Header = () => {
         className="flex gap-2 items-center"
         onClick={() => setTheme(theme)}
       >
-export default Header
+        <Icon icon={isDark ? sun : moonO} size={22} />
+        {isDark ? "Light Mode" : "Dark Mode"}
+      </button>
+    </header>
+  );
+};
+
+export default Header;
