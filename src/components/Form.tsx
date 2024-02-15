84bff8d825333505e222ced42758fc5a9154048f
Elyees Tatua <elyeestatua@gmail.com>
2024-02-15 21:27:23 +0300

    ops(Form): Creating the Form props function

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,10 +2,18 @@ import { Country } from "../templates/country";
 import { useEffect, useRef, useState } from "react";
 import { FormProps } from "./Home";
 
-const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+const Form: React.FC<FormProps> = ({ allCountries, setFilteredCountries }) => {
+  const searchInput = useRef<HTMLInputElement>(null);
+  const [searchState, setSearchState] = useState({
+    region: "all",
+    name: "",
+  });
+  
+  const handleSearch = () => {
+    const inputValue = searchInput?.current?.value
+      .trim()
+      .toLocaleLowerCase() as string;
+    setSearchState((prevState) => ({ ...prevState, name: inputValue }));
+  };
 
 export default Form
