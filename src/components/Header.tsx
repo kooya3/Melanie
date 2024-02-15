9777960e05bd6f19f8e0809bbbdf87b7ba661eac
Elyees Tatua <elyeestatua@gmail.com>
2024-02-15 21:41:34 +0300

    ops(Header): Working on the imports

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { DarkModeContext } from "../providers/DarkModeProvider";
 import { Icon } from "react-icons-kit";
 import { sun } from "react-icons-kit/icomoon/sun";
 import { moonO } from "react-icons-kit/fa/moonO";
-
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
