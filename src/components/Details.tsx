7030613fd6c89f03675343c9a62b46f604b9be01
Elyees Tatua <elyeestatua@gmail.com>
2024-02-15 20:58:28 +0300

    ops(Details): Working on imports

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,5 +1,8 @@
 import { useParams } from "react-router-dom";
 import { World } from "../templates/country";
 import { Link } from "react-router-dom";
+import { longArrowLeft } from "react-icons-kit/fa/longArrowLeft";
+import { Icon } from "react-icons-kit";
+
 
 export default Details
