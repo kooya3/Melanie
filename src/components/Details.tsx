a5a1e6abea738a8c983e86e90668e3f7d89386a3
Elyees Tatua <elyeestatua@gmail.com>
2024-02-15 21:02:46 +0300

    ops(Details): Working on imports

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -4,5 +4,5 @@ import { Link } from "react-router-dom";
 import { longArrowLeft } from "react-icons-kit/fa/longArrowLeft";
 import { Icon } from "react-icons-kit";
 import { ReactNode } from "react";
-
+import { setTitle } from "../helpers/setTitle";
 export default Details
