2bc27dbfc9695e33dd5f4ee9be32f60e41126efa
Elyees Tatua <elyeestatua@gmail.com>
2024-02-15 21:33:54 +0300

    ops(Form): Creating hooks to handle filtering

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -15,10 +15,23 @@ const Form: React.FC<FormProps> = ({ allCountries, setFilteredCountries }) => {
       .toLocaleLowerCase() as string;
     setSearchState((prevState) => ({ ...prevState, name: inputValue }));
   };
-  
+
   const filterByRegion = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSearchState((prevState) => ({ ...prevState, region: e.target.value }));
   };
+
+  useEffect(() => {
+    setFilteredCountries(
+      allCountries.filter((country) => {
+        return searchState.region == "all"
+          ? country.name.common.toLocaleLowerCase().startsWith(searchState.name)
+          : country.region.toLocaleLowerCase() === searchState.region &&
+              country.name.common
+                .toLocaleLowerCase()
+                .startsWith(searchState.name);
+      })
+    );
+  }, [searchState.name, searchState.region]);
 
 
 export default Form
