47e4e7a4336fa652e08c8f1ba0a665633f3c7cd4
Elyees Tatua <elyeestatua@gmail.com>
2024-02-15 21:47:51 +0300

    feat(Home): Created the functional component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Country } from "../templates/country";
+import Countries from "./Countries";
+import Form from "./Form";
+import Loading from "./Loading";
 
-const Home = () => {
+export type FormProps = {
+  allCountries: Country[];
+  setFilteredCountries: React.Dispatch<React.SetStateAction<Country[]>>;
+};
+export type CountriesProps = {
+  countries: Country[];
+  pages: number;
+};
+
+type loading = {
+  loading: boolean;
+  loadingError: boolean;
+};
+
+type Home = FormProps & CountriesProps & loading;
+
+const Home: React.FC<Home> = ({
+  allCountries,
+  setFilteredCountries,
+  countries,
+  pages,
+  loading,
+  loadingError,
+}) => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Form
+        allCountries={allCountries}
+        setFilteredCountries={setFilteredCountries}
+      />
+      {loadingError ? (
+        <div className="grid">
+          <p className="m-auto text-xl">Loading error. Try again.</p>
+        </div>
+      ) : loading ? (
+        <Loading />
+      ) : (
+        <Countries countries={countries} pages={pages} />
+      )}
+    </>
+  );
+};
 
-export default Home
+export default Home;
